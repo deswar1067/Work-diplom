0d93d4211577c2a6a437366e3b9960c0254e1ffa
deswar1067 <92688210+deswar1067@users.noreply.github.com>
2022-02-27 11:03:08 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,8 +11,8 @@ def browser():
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
-    driver = webdriver.Chrome(chrome_options=chrome_options)
-    # driver = webdriver.Chrome('/usr/bin/chromedriver')
+    # driver = webdriver.Chrome(chrome_options=chrome_options)
+    driver = webdriver.Chrome('/usr/bin/chromedriver')
     driver.maximize_window()
    
 
