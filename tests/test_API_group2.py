20cf1dd0379256d1672f8ff10c1c9b545e0a1fc8
deswar1067 <deswar1067@tut.by>
2022-02-28 15:12:41 +0300

    Готовый проект.

--- tests/test_API_group2.py
+++ tests/test_API_group2.py
@@ -12,7 +12,7 @@ def test_create_pet():
 
 def test_find_pets_id():
     """Поиск питомца"""
-    time.sleep(5)
+    time.sleep(6)
     response = find_pets_id("https://petstore.swagger.io/v2/pet/938")
     print(response.content)
     assert response.status_code == 200
@@ -20,14 +20,14 @@ def test_find_pets_id():
 
 def test_update_name_pet():
     """Обновление имени питомца"""
-    time.sleep(5)
+    time.sleep(6)
     response = update_name_pet("https://petstore.swagger.io/v2/pet/938", "swordfish")
     assert response.status_code == 200
 
 
 def test_search_name():
     """Поиск обновленного питомца"""
-    time.sleep(5)
+    time.sleep(6)
     response = update_name("https://petstore.swagger.io/v2/pet/938")
     name = json.loads(response.text)
     assert name["name"] in "swordfish"
@@ -35,6 +35,7 @@ def test_search_name():
 
 def test_delete_pet():
     """Удаление питомца"""
+    time.sleep(6)
     response = requests.delete("https://petstore.swagger.io/v2/pet/938")
     assert response.status_code == 200
 
