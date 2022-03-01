8086828c9af3fea029a937289c713d243216fe3a
deswar1067 <deswar1067@tut.by>
2022-03-01 23:12:16 +0300

    Готовый проект.

--- tests/test_group1_tc_2.py
+++ tests/test_group1_tc_2.py
@@ -18,12 +18,8 @@ def test_user_in_group(create_connection):
     cursor.execute("SELECT id FROM auth_user  where username = 'dikii_belarus'")  #вытаскиваем из таблицы
                                                                         # auth-user столбца dikii_belarus его айдишник
     a = cursor.fetchall()
-    # aa = [x[0] for x in a]
-    # print(aa)
     cursor.execute("SELECT user_id FROM auth_user_groups")
     b = cursor.fetchall()
-    # bb = [x[0] for x in b]
-    # print(bb)
     assert str(a) in str(b)
 
 
