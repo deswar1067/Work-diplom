323d7d7f135d20a732af1a8acef352f9207a9076
deswar1067 <deswar1067@tut.by>
2022-03-02 10:31:22 +0300

    Готовый проект.

--- tests/test_API_group1.py
+++ tests/test_API_group1.py
@@ -1,5 +1,3 @@
-import time
-
 from main.main_request_user import *
 
 
@@ -11,7 +9,6 @@ def test_create_user():
 
 def test_login_user():
     """Регистрация пользователя"""
-    # time.sleep(6)
     response = register_user("https://petstore.swagger.io/v2/user/login?username=deswar1067&password=123", 'deswar1067',
                              "123")
     print(response)
@@ -20,22 +17,18 @@ def test_login_user():
 
 def test_find_user():
     """Поиск пользователя"""
-    time.sleep(6)
-    get_data_user("https://petstore.swagger.io/v2/user/deswar1067", 'deswar1067')
     response = get_data_user("https://petstore.swagger.io/v2/user/deswar1067", 'deswar1067')
     assert response.status_code == 200
 
 
 def test_logout():
     """Выход пользователя из системы"""
-    # time.sleep(6)
     response = logout("https://petstore.swagger.io/v2/user/logout")
     assert response.status_code == 200
 
 
 def test_delete():
     """Удаление пользователя"""
-    # time.sleep(6)
     response = delete_user("https://petstore.swagger.io/v2/user/deswar1067")
     assert response.status_code == 200
 
